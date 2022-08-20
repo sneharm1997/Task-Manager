b959f223c0879599fa1fb6ecbb66b58906d032b6
sneha <sneharm1997@gmail.com>
2022-08-20 12:44:15 +0530

    enhancements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,31 +7,45 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TaskManagerComponent } from './components/task-manager/task-manager.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import { DialogContentComponent } from './components/dialog-content/dialog-content.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TaskManagerComponent
+    TaskManagerComponent,
+    DialogContentComponent,
+    FileUploadComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    FormsModule,
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
     DragDropModule,
     MatInputModule,
-    MatIconModule
-   
+    MatIconModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
